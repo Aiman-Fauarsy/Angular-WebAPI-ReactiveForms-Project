644a8235551dbf6c009322a40910dab1985d630b
Aiman <51406787+aimanfaw@users.noreply.github.com>
2019-09-09 22:48:31 +0300

    Add files via upload

---
+++ WebApi/NotesWEBAPI/NotesWEBAPI/Controllers/RegistersController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using NotesWEBAPI.Models;
+
+namespace NotesWEBAPI.Controllers
+{
+    public class RegistersController : ApiController
+    {
+        private NotesAppDataEntities db = new NotesAppDataEntities();
+
+        // GET: api/Registers
+        public IQueryable<Register> GetRegisters()
+        {
+            return db.Registers;
+        }
+
+        // GET: api/Registers/5
+        [ResponseType(typeof(Register))]
+        public async Task<IHttpActionResult> GetRegister(int id)
+        {
+            Register register = await db.Registers.FindAsync(id);
+            if (register == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(register);
+        }
+
+        // PUT: api/Registers/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutRegister(int id, Register register)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != register.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(register).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RegisterExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Registers
+        [ResponseType(typeof(Register))]
+        public async Task<IHttpActionResult> PostRegister(Register register)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Registers.Add(register);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = register.Id }, register);
+        }
+
+        // DELETE: api/Registers/5
+        [ResponseType(typeof(Register))]
+        public async Task<IHttpActionResult> DeleteRegister(int id)
+        {
+            Register register = await db.Registers.FindAsync(id);
+            if (register == null)
+            {
+                return NotFound();
+            }
+
+            db.Registers.Remove(register);
+            await db.SaveChangesAsync();
+
+            return Ok(register);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool RegisterExists(int id)
+        {
+            return db.Registers.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ WebApi/NotesWEBAPI/NotesWEBAPI/Controllers/RemindersController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Http.Description;
+using NotesWEBAPI.Models;
+
+namespace NotesWEBAPI.Controllers
+{
+    public class RemindersController : ApiController
+    {
+        private NotesAppDataEntities db = new NotesAppDataEntities();
+
+        // GET: api/Reminders
+        public IQueryable<Reminder> GetReminders()
+        {
+            return db.Reminders;
+        }
+
+        // GET: api/Reminders/5
+        [ResponseType(typeof(Reminder))]
+        public async Task<IHttpActionResult> GetReminder(int id)
+        {
+            Reminder reminder = await db.Reminders.FindAsync(id);
+            if (reminder == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(reminder);
+        }
+
+        // PUT: api/Reminders/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutReminder(int id, Reminder reminder)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != reminder.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(reminder).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ReminderExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Reminders
+        [ResponseType(typeof(Reminder))]
+        public async Task<IHttpActionResult> PostReminder(Reminder reminder)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Reminders.Add(reminder);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = reminder.Id }, reminder);
+        }
+
+        // DELETE: api/Reminders/5
+        [ResponseType(typeof(Reminder))]
+        public async Task<IHttpActionResult> DeleteReminder(int id)
+        {
+            Reminder reminder = await db.Reminders.FindAsync(id);
+            if (reminder == null)
+            {
+                return NotFound();
+            }
+
+            db.Reminders.Remove(reminder);
+            await db.SaveChangesAsync();
+
+            return Ok(reminder);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ReminderExists(int id)
+        {
+            return db.Reminders.Count(e => e.Id == id) > 0;
+        }
+    }
+}
